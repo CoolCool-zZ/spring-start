caae5057627e5a2e5768d33e5898b4598b32344b
coolcool <coolcool.with.you@gmail.com>
2024-01-16 21:32:57 +0900

    ADD - spotless apply

--- src/main/java/hello/springstart/SpringStartApplication.java
+++ src/main/java/hello/springstart/SpringStartApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringStartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringStartApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringStartApplication.class, args);
+  }
 }
