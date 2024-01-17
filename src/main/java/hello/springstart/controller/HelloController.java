5fe915617e88497becd99a2b76e29f8c667cadfd
coolcool <coolcool.with.you@gmail.com>
2024-01-17 21:53:26 +0900

    ADD - section2 spring web development basic

--- src/main/java/hello/springstart/controller/HelloController.java
+++ src/main/java/hello/springstart/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.springstart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,29 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello"; // hello.html로 연결
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(name = "name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(name = "name") String name) {
+        return Hello.from(name);
+    }
+
+    public record Hello(String name) {
+        public static Hello from(String name) {
+            return new Hello(name);
+        }
     }
 }
